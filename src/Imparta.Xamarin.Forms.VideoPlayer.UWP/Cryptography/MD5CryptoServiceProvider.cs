9d8d8da402fdcbd566b57e810bb01e98cf755e7f
Simon Price <simon.price@imparta.com>
2019-09-30 09:47:46 +0100

    First commit to App Controls Git Repo

---
+++ src/Imparta.Xamarin.Forms.VideoPlayer.UWP/Cryptography/MD5CryptoServiceProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace Imparta.Xamarin.Forms.VideoPlayer.UWP.Cryptography
+{
+    /// <summary>
+    /// Class MD5CryptoServiceProvider.
+    /// </summary>
+    internal class MD5CryptoServiceProvider : MD5
+    {
+
+    }
+}
